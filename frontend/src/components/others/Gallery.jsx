7c2b74534ed920720245a0f58e92cf9f8c06469a
FOA-developer <flourishagbanoma@gmail.com>
2026-07-07 16:31:48 +0100

    feat: add Gallery component with ScrollStack for image display

---
+++ frontend/src/components/others/Gallery.jsx
@@ -0,0 +1,31 @@
+import ScrollStack, { ScrollStackItem } from '../Animations/ScrollStack'
+
+const Gallery = ({img, img1, img2, title}) => {
+  return (
+    <section className="bg-secondary flex flex-col justify-start px-12">
+      <div>
+        <h4 className="text-xs md:text-sm text-text-main font-semibold tracking-[0.1em] uppercase text-left mt-16 ">Gallery</h4>
+        <h2 className="text-text-muted text-3xl font-semibold md:text-4xl pt-4">Moments from the {title}</h2>
+      </div>
+      <ScrollStack
+          useWindowScroll={true}
+          itemDistance={90}
+          stackPosition="18%"
+          itemStackDistance={20}
+          baseScale={0.9}
+        >
+          <ScrollStackItem>
+            <img src={img} alt="Gallery Image 1" className="w-full h-full object-cover rounded-[40px]" />
+          </ScrollStackItem>
+          <ScrollStackItem>
+            <img src={img1} alt="Gallery Image 2" className="w-full h-full object-cover rounded-[40px]" />
+          </ScrollStackItem>
+          <ScrollStackItem>
+            <img src={img2} alt="Gallery Image 3" className="w-full h-full object-cover rounded-[40px]" />
+          </ScrollStackItem>
+      </ScrollStack>
+    </section>
+  )
+}
+
+export default Gallery;
