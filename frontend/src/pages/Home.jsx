9119de6a42e763a99b7bef46f9e54da41112e732
FOA-developer <flourishagbanoma@gmail.com>
2026-07-07 14:08:45 +0100

    feat: add additional components to Home page for enhanced layout

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,21 @@
 import Hero from "../components/main/Hero";
 import Navbar from "../components/main/Navbar";
+import About from "../components/main/About";
+import Experience from "../components/main/Experience";
+import Work from "../components/main/Work";
+import Credentials from "../components/main/Credentials";
+import Contact from "../components/main/Contact";
 
 const Home = () => {
   return (
   <div>
     <Navbar />
     <Hero />
+    <About />
+    <Experience />
+    <Work />
+    <Credentials />
+    <Contact />
   </div>
   );
 };
