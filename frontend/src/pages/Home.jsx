30c43a5c4d72dfa88e0fbddbedb2ef3c29a67f69
FOA-developer <flourishagbanoma@gmail.com>
2026-07-07 16:53:37 +0100

    feat: integrate useScrollToHash hook for smooth scrolling functionality on Home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,8 +5,10 @@ import Experience from "../components/main/Experience";
 import Work from "../components/main/Work";
 import Credentials from "../components/main/Credentials";
 import Contact from "../components/main/Contact";
+import useScrollToHash from "../hooks/useScrollToHash";
 
 const Home = () => {
+  useScrollToHash();
   return (
   <div>
     <Navbar />
