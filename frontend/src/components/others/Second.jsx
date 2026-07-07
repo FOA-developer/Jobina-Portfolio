04dff320a7b28da795c8d53ebaf426f1943df874
FOA-developer <flourishagbanoma@gmail.com>
2026-07-07 16:32:08 +0100

    feat: add Second component for displaying project details with image and certification list

---
+++ frontend/src/components/others/Second.jsx
@@ -0,0 +1,25 @@
+const Second = ({img, title, description, cert}) => {
+  return (
+    <section>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-12 md:gap-8 px-8 md:p-12 mt-18">
+        <div className="flex flex-col items-start gap-4 overflow-hidden ">
+          <h4 className="text-xs md:text-sm text-text-main font-semibold tracking-[0.1em] uppercase text-left ">overview</h4>
+          <h2 className="text-text-muted text-3xl font-semibold md:text-4xl">What She Did</h2>
+          <p className="text-text-muted-soft text-sm md:text-base leading-relaxed">{description}</p>
+          <div>
+            {cert.map((c, index) => (
+              <div className="flex flex-row justify-between border-b-1 border-accent pt-4" key={index}>
+                <h4 className="text-text-muted-soft">- {c}</h4>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="flex flex-col justify-between items-start gap-2">
+          <img src={img} alt={title} className="object-cover w-full h-full rounded-xl" />
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Second;
