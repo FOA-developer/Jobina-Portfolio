dbb5881d8aa8314ebbdba8ca6c6c17e9ef507e15
FOA-developer <flourishagbanoma@gmail.com>
2026-07-08 17:24:54 +0100

    feat: update First component to accept 'first' prop for dynamic title

--- frontend/src/components/others/First.jsx
+++ frontend/src/components/others/First.jsx
@@ -1,14 +1,14 @@
 import Tag from "../shared/Tag";
 
 
-const First = ({tags, title, header, period,  description, link, img, impact }) => {
+const First = ({tags, title, first,  header, period,  description, link, img, impact }) => {
    return (
     <section className="">
       <div className="flex flex-col relative justify-between items-start overflow-hidden ">
         <img src={img} alt="Jobina Arinze" className="absolute bg-cover bg-center bg-no-repeat top-0 left-0 w-full h-full object-cover object-center"/>     
         <div className="absolute inset-0 bg-overlay"/>
         <div className="relative z-10 flex flex-col gap-4 justfy-between px-[10%] py-16">
-          <h4 className="text-xs md:text-sm text-text-main mt-20 tracking-[0.2em] uppercase text-left ">flagship programme</h4>
+          <h4 className="text-xs md:text-sm text-text-main mt-20 tracking-[0.2em] uppercase text-left ">{first}</h4>
           <h2 className="text-3xl text-white/90 font-semibold pt-6 leading-tight md:text-4xl max-w-3xl lg:text-5xl">{header}</h2>
           <p className=" text-sm text-white/90 md:text-base">{period}</p>
           <p className="text-white/60">{description}</p>
