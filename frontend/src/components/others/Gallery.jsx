bc99fa7874156bb96e7b9344f4c97e36cd0ad99b
FOA-developer <flourishagbanoma@gmail.com>
2026-07-08 17:25:17 +0100

    feat: extend Gallery component to include an additional image and update title rendering

--- frontend/src/components/others/Gallery.jsx
+++ frontend/src/components/others/Gallery.jsx
@@ -1,11 +1,11 @@
 import ScrollStack, { ScrollStackItem } from '../Animations/ScrollStack'
 
-const Gallery = ({img, img1, img2, title}) => {
+const Gallery = ({img, img1, img2, img3, title}) => {
   return (
     <section className="bg-secondary flex flex-col justify-start px-12">
       <div>
         <h4 className="text-xs md:text-sm text-text-main font-semibold tracking-[0.1em] uppercase text-left mt-16 ">Gallery</h4>
-        <h2 className="text-text-muted text-3xl font-semibold md:text-4xl pt-4">Moments from the {title}</h2>
+        <h2 className="text-text-muted text-3xl font-semibold md:text-4xl pt-4">{title}</h2>
       </div>
       <ScrollStack
           useWindowScroll={true}
@@ -22,6 +22,9 @@ const Gallery = ({img, img1, img2, title}) => {
           </ScrollStackItem>
           <ScrollStackItem>
             <img src={img2} alt="Gallery Image 3" className="w-full h-full object-cover rounded-[40px]" />
+          </ScrollStackItem>
+          <ScrollStackItem>
+            <img src={img3} alt="Gallery Image 3" className="w-full h-full object-cover rounded-[40px]" />
           </ScrollStackItem>
       </ScrollStack>
     </section>
