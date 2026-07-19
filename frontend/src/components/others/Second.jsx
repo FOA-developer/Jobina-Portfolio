2a72082607cdaaa104b4dab44c6290115983c31a
FOA-developer <flourishagbanoma@gmail.com>
2026-07-19 03:12:12 +0100

    feat: added additional description field to Second component for enhanced content display

--- frontend/src/components/others/Second.jsx
+++ frontend/src/components/others/Second.jsx
@@ -1,11 +1,11 @@
-const Second = ({img, title, description, cert}) => {
+const Second = ({img, title, description, description1, cert}) => {
   return (
     <section>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-12 md:gap-8 px-8 mb-15 md:p-12 mt-18">
         <div className="flex flex-col items-start gap-4 overflow-hidden ">
           <h4 className="text-xs md:text-sm text-text-main font-semibold tracking-[0.1em] uppercase text-left ">overview</h4>
           <h2 className="text-text-muted text-3xl font-semibold md:text-4xl">What She Did</h2>
-          <p className="text-text-muted-soft text-sm md:text-base leading-relaxed">{description}</p>
+          <p className="text-text-muted-soft text-sm md:text-base leading-relaxed">{description} <br></br><br></br>{description1}</p>
           <div>
             {cert.map((c, index) => (
               <div className="flex flex-row justify-between border-b-1 border-accent pt-4" key={index}>
