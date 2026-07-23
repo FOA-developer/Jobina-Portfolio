7a7688e6949c769c20ed5016bf04e0aecea20750
FOA-developer <flourishagbanoma@gmail.com>
2026-07-23 16:22:52 +0100

    fix: removed routes of deleted pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,11 +6,7 @@ import Jobberman from './pages/Jobber'
 import Girl from './pages/Girl'
 import Jci from './pages/Jci';
 import Royal from './pages/Royal'  
-import Power from './pages/Power';
-import Aws from './pages/Aws';
-import Digital from './pages/Digital';
 import Blind from './pages/Blind';
-import Excel from './pages/Excel';
 import Pathway from './pages/Pathway';
 import ScrollToTop from './components/others/ScrollToTop';
 import Edo from './pages/Edo';
@@ -28,12 +24,8 @@ function App() {
         <Route path="/gritin_ai_conference" element={<Grit />} />
         <Route path="/girllead" element={<Girl />} />
         <Route path="/jcin-uniben" element={<Jci />} />
-        <Route path="/data_analytics" element={<Power />} />
         <Route path="/royaliwere" element={<Royal />} />
-        <Route path="/aws_restart" element={<Aws />} />
-        <Route path="/digital_literacy" element={<Digital />} />
         <Route path="/tech_inclusion_for_the_blind" element={<Blind />} />
-        <Route path="/excel_essentials" element={<Excel />} />
         <Route path="/pathway_to_the_economy" element={<Pathway />} />
         <Route path="/edobessto" element={<Edo />} />
         <Route path="/se4g" element={<Seg />} />
